038d05c605b8741639ea56d5106d67a352f34977
giuliacajati <giuliacajati@gmail.com>
2020-10-08 18:53:23 -0400

    Done.

---
+++ dist/index.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  updateDOM();
+});
+
+function updateDOM() {
+  document.getElementById("text").innerHTML = "This is really cool!";
+}
